fa6f8fe10f680cd75b70a45da510cde0f3473d35
Hatri <18404171+Hatri@users.noreply.github.com>
2020-06-28 22:07:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { fetchWeather } from './api/fetchWeather';
+import './App.css';
+
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
     return (
-        <h1>App</h1>
+        <div className="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="Search ..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+
+
+            />
+        </div>
     );
 }
 
